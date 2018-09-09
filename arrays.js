2286324c259bfea4f65c22bedc156051112d48bf
Julian Brown <julianxbrown@gmail.com>
2018-09-09 15:53:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,7 @@
+// define a variable called chocolateBars. Its value should be an array of the strings snickers, hundred grand, kitkat, and skittles.
+
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
 
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
